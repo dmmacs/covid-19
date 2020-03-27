3172ae354c4aca677d65109bb4e4ce1aaddd7381
Don McDonald <dmmacs@gmail.com>
2020-03-26 21:27:14 -0700

    Initial Pass

---
+++ covid-19.py
@@ -0,0 +1,22 @@
+
+
+
+import selenium
+import requests
+
+# with open("tmp.txt", "r") as fin:
+#     urlData = fin.read()
+
+url = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html"
+#result = requests.request("GET", url)
+#urlData = result.text
+
+#with open("tmp.txt", "w", encoding="utf-8") as out:
+#    out.write(result.text)
+
+#idx = urlData.find("<table id=\"cdc-chart-1-data\"")
+
+#print(idx)
+
+headers =
+page = requests.get()
