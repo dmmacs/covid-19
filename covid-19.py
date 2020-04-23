58e41c40ec2e1b72ff8a37c3be056e7a4d4f228c
Don McDonald <dmmacs@gmail.com>
2020-04-23 10:47:45 -0700

    Fixed to use new CDC data site and update to better correlate death data and cases data.

--- covid-19.py
+++ covid-19.py
@@ -1,25 +1,91 @@
 #! /home/dmmacs/anaconda3/bin/python3
 
 
-#import selenium
+from bs4 import BeautifulSoup
 import requests
+from requests.exceptions import RequestException
 import json
 from datetime import datetime
 import platform
 import pytz
+import csv
+
+from contextlib import closing
+from requests import get
 
 
 def getMonth(s):
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     return months.index(s)+1
 
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+def ScrapeData():
+
+    # with open("contrived.html", "r", encoding="utf-8") as fin:
+    #     html = BeautifulSoup(fin, 'html.parser')
+    # for p in html.select('p'):
+    #     print(p)
+    #     if p['id'] == 'walrus':
+    #         print(p.text)
+    # return
+
+    data = simple_get("https://ourworldindata.org/grapher/daily-cases-covid-19")
+    html = BeautifulSoup(data, 'html.parser')
+
+    for bullet in html.select('li'):
+        # if bullet['class'] == "tab clickable":
+        print(bullet)
+    # for a in html.select('a'):
+    #     if bullet['class'] == "data-track-click":
+    #         print(a)
+
+
+
 if __name__ == "__main__":
-    
+
+    # ScrapeData()
+    # exit(0)
+
     deathUrl = "https://www.worldometers.info/coronavirus/country/us/"
     url = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html"
     dailyUrl = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/us-cases-epi-chart.json"
     totalUrl = "https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/total-cases-onset.json"
-
+    totalUrl = "https://www.cdc.gov//coronavirus/2019-ncov/json/cumm-total-chart-data.json"
 
     result = requests.request('GET', deathUrl)
 
@@ -63,29 +129,31 @@ if __name__ == "__main__":
     idx = rawData.find("series",idx)
     result = requests.request("GET", totalUrl)
     virus_total_data = result.json()
-    result = requests.request("GET", dailyUrl)
-    virus_daily_data = result.json()
+    
+    # result = requests.request("GET", dailyUrl)
+    # virus_daily_data = result.json()
 
-    columns = virus_daily_data['data']['columns'][0]
+    # columns = virus_daily_data['data']['columns'][0]
 
     daily_data = []
     total_data = []
-    for i, col in enumerate(columns):
-        if i >= 1:
-            daily_data.append([col,0,0,0])
-        # print(daily_data[-1])
+    # for i, col in enumerate(columns):
+    #     if i >= 1:
+    #         daily_data.append([col,0,0,0])
+    #     # print(daily_data[-1])
 
-    vData = virus_daily_data['data']['columns'][1]
+    # vData = virus_daily_data['data']['columns'][1]
 
-    total = 0
-    for i,dat in enumerate(vData):
-        if i >= 1:
-            total += int(dat)
-            daily_data[i-1] = [daily_data[i-1][0], dat, total,0]
-            # print(daily_data[i])
+    # total = 0
+    # for i,dat in enumerate(vData):
+    #     if i >= 1:
+    #         total += int(dat)
+    #         daily_data[i-1] = [daily_data[i-1][0], dat, total,0]
+    #         # print(daily_data[i])
 
-    columns = virus_total_data['data']['columns'][0]
-    vData = virus_total_data['data']['columns'][1]
+    #columns = virus_total_data['data']['columns'][0]
+    columns = virus_total_data[0]
+    vData = virus_total_data[1]
 
     for i, col in enumerate(columns):
         if i >= 1:
@@ -95,23 +163,27 @@ if __name__ == "__main__":
         if i >= 1:
             total_data[i-1] = [total_data[i-1][0], dat,0]
 
-    for i, total_dat in enumerate(total_data):
-        for j, dailyDat in enumerate(daily_data):
-            if dailyDat[0] == total_dat[0]:
-                daily_data[j] = [daily_data[j][0],daily_data[j][1],daily_data[j][2],total_dat[1]]
+    # for i, total_dat in enumerate(total_data):
+    #     for j, dailyDat in enumerate(daily_data):
+    #         if dailyDat[0] == total_dat[0]:
+    #             daily_data[j] = [daily_data[j][0],daily_data[j][1],daily_data[j][2],total_dat[1]]
 
     for i, dat in enumerate(total_data):
+        tDate = datetime.strptime(dat[0],"%m/%d/%Y")
         for j, dDate in enumerate(deathDates):
-            if platform.system() == "Windows":
-                if dat[0] == dDate.strftime("%#m/%#d/%Y"):
-                    # print(dat)
-                    total_data[i] = [total_data[i][0], total_data[i][1],deathData[j]]
-                    break
-            else:
-                if dat[0] == dDate.strftime("%-m/%-d/%Y"):
-                    # print(dat)
-                    total_data[i] = [total_data[i][0], total_data[i][1],deathData[j]]
-                    break
+            if (tDate-dDate).days == 0:
+                total_data[i] = [total_data[i][0], total_data[i][1],deathData[j]]
+            # if platform.system() == "Windows":
+            #     if dat[0] == dDate.strftime("%#m/%#d/%Y"):
+            #         # print(dat)
+            #         total_data[i] = [total_data[i][0], total_data[i][1],deathData[j]]
+            #         break
+            # else:
+            #     if dat[0] == dDate.strftime("%-m/%-d/%Y"):
+            #         # print(dat)
+            #         total_data[i] = [total_data[i][0], total_data[i][1],deathData[j]]
+            #         break
+
 
     # Create data1.js file
     out_data = ""
@@ -140,7 +212,7 @@ if __name__ == "__main__":
 
     try:
         with open("data1.js", "w", encoding="UTF-8") as out:
-            out.write(out_data)
+            # out.write(out_data)
             out.write(out_data1)
             now = datetime.now()
             out.write("updateTime=" + "\"" + now.astimezone(tz=AZ_TZ).strftime('%d-%b-%Y %I:%M:%S %p %Z') + "\"\n")
