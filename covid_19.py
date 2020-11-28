678ee4a37aa2a17ce8a0a452352973fc3b54607c
Don McDonald <dmmacs@gmail.com>
2020-11-28 10:20:45 -0700

    Code Clean up

--- covid_19.py
+++ covid_19.py
@@ -29,136 +29,6 @@ from contextlib import closing
 
 Pop_data = {}
 
-# def getRapidApiData():
-
-#     print("Running Rapid Data")
-#     fname = "jsonData.txt"
-#     # Load Saved Data First
-#     if os.path.exists(fname):
-#         with open(fname,"r", encoding="UTF-8") as fin:
-#             jsonData = json.load(fin)
-#     fname1 = "az.txt"
-#     if os.path.exists(fname1):
-#         with open(fname1,"r", encoding="UTF-8") as fin:
-#             jsonDataAZ = json.load(fin)
-
-
-#     # Get Date from last entry
-#     # lastFDate = list(jsonData[-1].keys())[0]
-
-#     # lastDate = datetime.strptime(lastFDate,"%Y-%m-%d")
-
-#     now = datetime.now()
-#     # today = datetime(year=now.year, month=now.month, day=now.day,hour=0, minute=0, second=0)
-
-#     url = "https://covid-19-data.p.rapidapi.com/report/country/name"
-#     headers = {
-#         'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
-#         'x-rapidapi-key': "017581f6cfmshc8f39f291c879dfp1140dejsn60deff78a7b2"
-#         }
-
-#     url = "https://covid-19-data.p.rapidapi.com/country/code"
-#     querystring = {"format":"json","code":"USA"}
-#     response = request("GET", url, headers=headers, params=querystring)
-
-#     fileDateStr = ""
-#     if response.status_code == 200:
-#         tmpjsonData = response.json()
-#         dateStr = f"{now:%Y-%m-%d}"
-#         totalCases = tmpjsonData[0]['confirmed']
-#         totalDeaths = tmpjsonData[0]['deaths']
-
-#         tmpjsonData = jsonData[-1]
-#         if dateStr == list(tmpjsonData.keys())[0]:
-#             jsonData[-1] = {dateStr:{"cases":totalCases,"deaths":totalDeaths}}
-#         else:
-#             jsonData.append({dateStr:{"cases":totalCases,"deaths":totalDeaths}})
-
-#     url2 = "https://covid-19-data.p.rapidapi.com/report/country/name"
-#     querystring = {"format":"json","code":"USA"}
-#     yesterday = now + timedelta(days=-1)
-#     dateStr = f"{yesterday:%Y-%m-%d}"
-# #    dateStr = f"{now:%Y-%m-16}"
-#     querystring = {"date-format":"YYYY-MM-DD","format":"json","date":dateStr,"name":"USA"}
-#     response = request("GET", url2, headers=headers, params=querystring)
-#     if response.status_code == 200:
-#         tmpjsonData = response.json()
-#         dateStr = f"{yesterday:%Y-%m-%d}"
-#         totalCases = 0
-#         totalDeaths = 0
-#         totalRecovered = 0
-#         for prov in tmpjsonData[0]['provinces']:
-#             if prov['province'] == 'Arizona':
-#                 totalCases += prov['confirmed']
-#                 totalDeaths += prov['deaths']
-#                 totalRecovered += prov['recovered']
-#                 break
-#         if totalCases > 0:
-#             tmpjsonData = jsonDataAZ[-1]
-#             if dateStr == list(tmpjsonData.keys())[0]:
-#                 jsonDataAZ[-1] = {dateStr:{"cases":totalCases,"deaths":totalDeaths}}
-#             else:
-#                 jsonDataAZ.append({dateStr:{"cases":totalCases,"deaths":totalDeaths}})
-
-#     # Create data1.js file
-#     out_data1 = "//Rapid API Data\n"
-#     out_data1 += "row_dataUS = [\n"
-#     for entry in jsonData:
-#         #print(entry)
-#         key = list(entry.keys())[0]
-#         out_data1 += "\t[\"" + key  + "\"" 
-#         out_data1 += ","
-#         out_data1 += str(entry.get(key).get("cases"))
-#         out_data1 += ","
-#         out_data1 += str(entry.get(key).get("deaths"))
-#         out_data1 += "],\n" # + "\"," + str(row[1]) + "," + str(row[2]) + "],\n"
-
-#     out_data1 += "];\n"
-
-#     # Create data1.js file
-#     out_data1 += "//Rapid API Data\n"
-#     out_data1 += "row_dataAZ = [\n"
-#     for entry in jsonDataAZ:
-#         #print(entry)
-#         key = list(entry.keys())[0]
-#         out_data1 += "\t[\"" + key  + "\"" 
-#         out_data1 += ","
-#         out_data1 += str(entry.get(key).get("cases"))
-#         out_data1 += ","
-#         out_data1 += str(entry.get(key).get("deaths"))
-#         out_data1 += "],\n" # + "\"," + str(row[1]) + "," + str(row[2]) + "],\n"
-
-#     out_data1 += "];\n"
-
-#     # UTC_TZ = pytz.timezone('UTC')
-#     # Eastern_TZ = pytz.timezone("US/Eastern")
-#     # Mountain_TZ = pytz.timezone("US/Mountain")
-#     AZ_TZ = pytz.timezone("US/Arizona")
-    
-#     try:
-#         with open("data1.js", "w", encoding="UTF-8") as out:
-#             # out.write(out_data)
-#             out.write(out_data1)
-#             now = datetime.now()
-#             out.write("updateTime=" + "\"" + now.astimezone(tz=AZ_TZ).strftime('%d-%b-%Y %I:%M:%S %p %Z') + "\"\n")
-#     except Exception as exc:
-#         print(exc)
-#         exit(-1)
-
-
-    
-#     with open(fname, "w", encoding="UTF-8") as fout:
-#         json.dump(jsonData, fout, indent=4)
-
-
-
-#     fileDateStr = f"{now:%Y-%m-%d_%H%M%S}"
-
-#     shutil.copyfile(fname1, fileDateStr + "-" + fname1)
-
-#     with open (fname1, "w", encoding="UTF-8") as fout:
-#         json.dump(jsonDataAZ, fout, indent=4)
-
 def getCovidTrackData(area):
 
     print("Running CovidTracking Data for " + area)
@@ -170,8 +40,6 @@ def getCovidTrackData(area):
         if response.status_code == 200:
             tmpData = response.json()
             # print(json.dumps(response.json(), indent=4))
-            totalCases = tmpData[0]['positive']
-            totalDeaths = tmpData[0]['death']
             tmpStr = str(tmpData[0]['date'])
             dateStr = tmpStr[0:4] + "-" + tmpStr[4:6] + "-" + tmpStr[6:8]
             
@@ -186,8 +54,6 @@ def getCovidTrackData(area):
         if response.status_code == 200:
             tmpData = response.json()
             # print(json.dumps(response.json(), indent=4))
-            totalCases = tmpData['positive']
-            totalDeaths = tmpData['death']
             tmpStr = str(tmpData['date'])
             dateStr = tmpStr[0:4] + "-" + tmpStr[4:6] + "-" + tmpStr[6:8]
             
@@ -211,12 +77,8 @@ def createOutData(jsonData, varName):
     
     test_pos_vals = []
     test_neg_vals = []
-    prev_tests = 0
-    test_sum = 0
 
     mv_ave_data = []
-    prev_cases = 0
-    case_sum = 0
     
     ave_window = 7
     
@@ -230,15 +92,12 @@ def createOutData(jsonData, varName):
         # Date (index 0)
         OutStr += "\t[\"" + key  + "\"" 
         OutStr += ","
-        cases = 0
-        tests = 0
 
         # Cases (index 1), positive tests
         if entry.get(key).get("positive") is None:
             OutStr += str(0)
         else:
             OutStr += str(entry.get(key).get("positive"))
-            cases = entry.get(key).get("positive")
         OutStr += ","
         
         # Deaths (index 2)
@@ -271,9 +130,6 @@ def createOutData(jsonData, varName):
 
         
         # 7 day positivity Average (index 5)
-        # test_pos_vals = []
-        # test_neg_vals = []
-
         if len(test_pos_vals) == ave_window:
             test_pos_vals.pop(0)
             test_neg_vals.pop(0)
